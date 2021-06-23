ee7bd7b0bbfb6c51dd439348e19e0888462ebb69
ulldecorb <ausias.bertran@gmail.com>
2021-06-23 02:28:46 +0200

    Build theme-detail component

--- client/src/components/5b-Theme-detail/index.tsx
+++ client/src/components/5b-Theme-detail/index.tsx
@@ -12,11 +12,41 @@ const ThemesDetail = () => (
           THEME DETAIL
         </h2>
         <div className="box-detail__description">
+          <p className="box-detail__input-test">Input rigth here</p>
           <input
             type="text"
             className="box-detail__input"
             placeholder="Enter description"
           />
+        </div>
+        <div className="box-detail__description">
+          <p className="box-detail__input-test">description</p>
+          <input
+            type="text"
+            className="box-detail__input"
+            placeholder="Enter description"
+          />
+        </div>
+        <div className="box-detail__description">
+          <p className="box-detail__input-test">notes/abouts of</p>
+          <input
+            type="text"
+            className="box-detail__input"
+            placeholder="Enter description"
+          />
+        </div>
+        <div className="links-box">
+          <div className="links-box__link">
+            <h3>link title</h3>
+            <div className="links-box__link-button">
+              url title
+            </div>
+            <img
+              src="https://image.flaticon.com/icons/png/512/817/817808.png"
+              alt="whould wide world"
+              className="links-box__image"
+            />
+          </div>
         </div>
       </div>
     </div>
